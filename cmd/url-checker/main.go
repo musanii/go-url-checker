da4adc274a1fd96537d34404fdad146c80df9c65
Kevin Musanii <kibetmusanii@gmail.com>
2026-08-18 14:35:03 +0300

    feat: check URLs from CLI

--- cmd/url-checker/main.go
+++ cmd/url-checker/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
+
+	"github.com/musanii/go-url-checker/internal/checker"
 )
 
 func run(args []string) error {
 	if len(args) == 0 {
 		return errors.New("at least one URL is required")
+	}
+	results := checker.CheckMultipleURLs(args)
+
+	for _,result := range results{
+		fmt.Printf(
+			"%s %d\n",
+			result.URL,
+			result.StatusCode,
+		)
 	}
 	return nil
 }
--- cmd/url-checker/main_test.go
+++ cmd/url-checker/main_test.go
@@ -1,22 +1,85 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
 
-func TestRunRequiresURLs(t *testing.T){
+func TestRunRequiresURLs(t *testing.T) {
 	err := run([]string{})
 
-	if err ==nil{
+	if err == nil {
 		t.Fatal("expected URL argument error, got nil")
 	}
 
 	expected := "at least one URL is required"
 
-	if err.Error() != expected{
+	if err.Error() != expected {
 		t.Fatalf(
 			"expected error %q, got %q",
 			expected,
 			err.Error(),
 		)
 
+	}
+}
+
+func captureOutput(fn func()) string {
+	old := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	fn()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	return buf.String()
+}
+
+func TestRunChecksURLs(t *testing.T) {
+	server := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}),
+	)
+
+	defer server.Close()
+
+	output := captureOutput(func() {
+		err := run([]string{server.URL})
+
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+	})
+
+	if !strings.Contains(output, server.URL) {
+		t.Fatalf(
+			"expected output to contain %q, got %q",
+			server.URL,
+			output,
+		)
+	}
+
+	if !strings.Contains(output, "200") {
+		t.Fatalf(
+			"expected output to contain status 200, got %q",
+			output,
+		)
+	}
+
+	err := run([]string{server.URL})
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
 	}
 }
